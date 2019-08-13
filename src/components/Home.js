9fc9e053df43990830240adccb91e080c8e937bd
Diane Sanchez <diane@Dianes-MacBook-Air-2.local>
2019-08-13 15:34:57 -0600

    created home and nav bar components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Nav';
+
+function Home() {
+    return (
+      <div className="Home">
+       <p>Test</p> 
+      </div>
+    );
+  }
+
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <div className="Nav">
+        <h1>NAV</h1>
+      </div>
+    );
+  }
+
+export default Nav;
